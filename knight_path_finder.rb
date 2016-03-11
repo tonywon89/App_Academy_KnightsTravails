a6aa7599c94fa90a944db2300c4abf34936a6b8d
Tony Won <tonywon89@gmail.com>
2016-03-11 15:13:27 -0800

    Added valid moves

---
+++ knight_path_finder.rb
@@ -0,0 +1,45 @@
+require_relative 'poly_tree_node'
+
+class KnightPathFinder
+  DELTAS =[
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1]
+  ]
+
+  def self.valid_moves(pos)
+    row, col = pos
+    possible_moves = DELTAS.select do |d_row, d_col|
+      new_row = row + d_row
+      new_col = col + d_col
+      new_row.between?(0, 7) && new_col.between?(0, 7)
+    end
+    possible_moves.map {|d_row, d_col| [d_row + row, d_col + col]  }
+  end
+
+  # def self.valid_moves(pos)
+  #   possible_moves = []
+  #   row, col = pos
+  #   DELTAS.each do |delta|
+  #     d_row, d_col = delta
+  #     possible_move = [row + d_row, col + d_col]
+  #     possible_moves << possible_move if valid_pos?(possible_move)
+  #   end
+  #   possible_moves
+  # end
+  #
+  # def self.valid_pos?(pos)
+  #   row, col = pos
+  #   row.between?(0,7) && col.between?(0,7)
+  # end
+
+  def initialize(pos = [0, 0])
+
+  end
+
+end
---
+++ poly_tree_node.rb
@@ -0,0 +1,86 @@
+class PolyTreeNode
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent
+    @parent
+  end
+
+def root_node?
+  parent.nil?
+end
+
+  # def assign_parent(parent)
+  #   @parent = parent
+  #   parent.children
+  # end
+  def parent=(new_parent)
+    #Alternative way: @parent.children.reject! { |child| child == self}
+    parent.children.delete(self) unless self.root_node?
+    @parent = new_parent
+    new_parent.children << self unless new_parent.nil?
+  end
+
+  def children
+    @children
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    if children.include?(child_node)
+      child_node.parent = nil
+    else
+      raise "Error! Parent does not have this child."
+    end
+  end
+
+  def value
+    @value
+  end
+
+  def inspect
+    @value.inspect
+  end
+
+  def dfs(target_value)
+    return self if value == target_value
+    children.each do |child|
+      #Rather than making two recursive calls, we save the return value of the recursive call in a variable, and then (do stuff) with that variable.
+       result = child.dfs(target_value)
+       return result unless result.nil?
+    end
+    nil
+  end
+
+  # def dfs(target_value)
+  #   return self if value == target_value
+  #   return nil if children.empty?
+  #   children.each do |child|
+  #      child.dfs(target_value)
+  #   end
+  # end
+
+
+  def bfs(target_value)
+    queue = []
+
+    queue.push(self)
+
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == target_value
+      current_node.children.each do |child|
+        queue.push(child)
+      end
+    end
+    nil
+  end
+
+end
